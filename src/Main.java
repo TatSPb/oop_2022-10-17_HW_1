6f6fcdeff67e4749f9f94d19b04a411dde38c2dc
TatSPb <t-sverchkova@yandex.ru>
2023-01-27 21:04:41 +0300

    Commit HW part 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import Products.Product;
+
+import static Products.ProductList.printProductList;
+import static Products.ProductList.productList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Product cucumber = new Product("Огурцы", 150.00, 0.3);
+        Product tomato = new Product("Помидоры", 250.00, 0.3);
+        Product sweetPepper = new Product("Перец болгарский", 250.00, 0.3);
+        Product dill = new Product("Укроп", 300.00, 0.03);
+        Product oliveOil = new Product("Масло оливковое", 1000.00, 0.05);
+
+        printProductList();
     }
 }
---
+++ src/Products/ProductList.java
@@ -0,0 +1,25 @@
+package Products;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProductList {
+
+    public static Set<Product> productList = new HashSet<>();
+
+    public static void addProductToList(Product newProduct) {
+        if (!productList.add(newProduct)) {
+            throw new RuntimeException("Данный продукт уже есть в списке");
+        }
+    }
+
+    public static void printProductList() {
+        System.out.println("СПИСОК ПРОДУКТОВ: ");
+
+        for (Product product : productList) {
+            System.out.println(product.getName() + ", цена: " + product.getPrice() + " руб/кг," + " требуемое количество: "+ product.getQuantity() + " кг.");
+        }
+    }
+
+
+}
